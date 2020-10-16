c91a70326f34b5d17ad80f2b90546e00da7c08b3
MrLeonKeNN <61803026+MrLeonKeNN@users.noreply.github.com>
2020-10-16 13:18:44 +0300

    one

---
+++ js/js.js
@@ -0,0 +1,32 @@
+// document.getElementById('but').onclick = func1();
+function reze() {
+	let element = document.getElementById('but');
+	element.classList.add("geroi");
+}
+function reze1(){
+	let element = document.getElementById('but');
+	element.classList.remove("geroi")
+}
+function reze3(){
+	let element = document.getElementById('but');
+	if (element.classList.contains('geroi')){
+		reze1();
+	}else{
+		reze();
+	}
+}
+
+function burger(){
+	let elBody = document.getElementById('body');
+	let element = document.getElementById('menu');
+	if(element.classList.contains('burger__menu__off')){
+		element.classList.remove('burger__menu__off');
+		elBody.classList.remove('lock');
+	}else{
+		element.classList.add('burger__menu__off');
+		elBody.classList.add('lock');
+	}
+}
+burger1.onclick = burger;
+but.onclick = reze3;
+
